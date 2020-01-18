fff660bc6eb47f79f890ce6c712aeee99e299fa6
TOPPON <pont717@gmail.com>
2020-01-18 22:35:04 +0900

    【Add】プロジェクト追加、ボールやプレイヤーを追加

---
+++ SOCCER/Assets/Scripts/player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    Vector2 movevec;
+    float speed;
+    float Lasttime;
+    Rigidbody2D rb;
+    enum Team
+    {
+        Red,Blue
+    }
+    Team myTeam ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(1.0f,1.5f);
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+        if (Random.Range(1, 2) == 1) SetTeam(1);
+        else SetTeam(2);
+    }
+    void SetTeam(int Teams)
+    { 
+        if (Teams == 1) myTeam = Team.Red;
+        if (Teams == 2) myTeam = Team.Blue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Lasttime > 0) Lasttime -= Time.deltaTime;
+        else
+        {
+            movevec = Random.insideUnitCircle.normalized;
+            Lasttime = Random.Range(1.5f,3.0f);
+            rb.velocity = movevec * speed;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ball"))
+        {
+            soccerball temp=collision.gameObject.GetComponent<soccerball>();
+            temp.Kick(2,new Vector2((int )(myTeam)*2-1,0));
+        }
+    }
+}
---
+++ SOCCER/Assets/Scripts/soccerball.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soccerball : MonoBehaviour
+{
+    float speed;
+    Vector2 movevec;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+        movevec = new Vector2(0.7f, 0.7f);
+        speed = 3;
+    }
+
+    public void Kick(float addspeed, Vector2 addvec)
+    {
+        Vector2 a = speed * movevec + addspeed * addvec;
+        speed = a.magnitude;
+        movevec = a.normalized;
+    }
+    void Refresh()
+    {
+        Vector2 a = rb.velocity;
+        speed = a.magnitude;
+        movevec = a.normalized;
+        rb.velocity=movevec*speed;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Refresh();
+        rb.velocity = movevec * speed;
+        if (speed > 0)
+        {
+            speed -= Time.deltaTime*2;
+        }
+        else speed = 0;
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            Kick(3, new Vector2(Random.Range(-1.0f,1.0f), Random.Range(-1.0f, 1.0f)));
+        }
+    }
+}
